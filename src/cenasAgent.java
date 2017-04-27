7626d3cc4260deea3a0d6b590a23faaccbaec005
Unknown <jlima91@hotmail.com>
2017-04-27 15:41:18 +0100

    1

---
+++ src/cenasAgent.java
@@ -0,0 +1,12 @@
+import jade.core.Agent;
+
+/**
+ * Created by joselima on 27/04/17.
+ */
+public class cenasAgent extends Agent {
+
+    protected void Setup(){
+        System.out.println("agente fixe");
+        doDelete();
+    }
+}
